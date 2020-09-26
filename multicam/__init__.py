0b62c3030803b9d531e5525a6d2b181e7e2382ba
Najam Syed <najam.r.syed@gmail.com>
2020-09-26 15:10:20 -0400

    Make package. Rename threadcam.py -> multicam.py

---
+++ multicam/__init__.py
@@ -0,0 +1,3 @@
+from .multicam import get_cam_ids, show_videos
+
+__all__ = ["get_cam_ids", "show_videos"]
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="multithread-multicam",
+    version="0.1",
+    url="https://github.com/nrsyed/multithread-multicam",
+    author="Najam R. Syed",
+    author_email="najam.r.syed@gmail.com",
+    license="MIT",
+    packages=["multicam"],
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "screeninfo"
+    ],
+    entry_points={
+        "console_scripts": ["multicam = multicam.__main__:main"],
+    },
+)
